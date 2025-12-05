f3942764073352c9dca76a4dee707dc08d74ea05
Joyee Sen <sen.joyee@gmail.com>
2025-12-05 16:58:22 +0000

    chore: remove unused utils modules and kpi extractor test”

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,25 +1,3 @@
 """Utility modules for the EWA Analyzer backend."""
 
-from utils.date_utils import parse_date_any, format_date_display, format_date_iso
-from utils.ewa_helpers import (
-    to_title_case,
-    escape_html,
-    merge_findings_and_recommendations,
-    normalize_severity,
-    get_severity_color,
-    format_effort,
-)
-
-__all__ = [
-    # date_utils
-    "parse_date_any",
-    "format_date_display",
-    "format_date_iso",
-    # ewa_helpers
-    "to_title_case",
-    "escape_html",
-    "merge_findings_and_recommendations",
-    "normalize_severity",
-    "get_severity_color",
-    "format_effort",
-]
+__all__: list[str] = []
